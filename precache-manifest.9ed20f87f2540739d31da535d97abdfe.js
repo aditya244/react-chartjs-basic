9d70ddd7a3de60d0cd578d442d037dc0e63652a1
Aditya <sinha.aditya244@gmail.com>
2020-05-06 14:01:24 +0530

    Updates

---
+++ precache-manifest.9ed20f87f2540739d31da535d97abdfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a88078ee8782af7d1ddc6253b1e049",
+    "url": "/react-chartjs-basic/index.html"
+  },
+  {
+    "revision": "f32c908015b5cccc159a",
+    "url": "/react-chartjs-basic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dcc711de463adc5da31",
+    "url": "/react-chartjs-basic/static/js/2.b67d6a19.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/react-chartjs-basic/static/js/2.b67d6a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32c908015b5cccc159a",
+    "url": "/react-chartjs-basic/static/js/main.1063120f.chunk.js"
+  },
+  {
+    "revision": "4aa7edb1113545925c9c",
+    "url": "/react-chartjs-basic/static/js/runtime-main.82266998.js"
+  }
+]);
