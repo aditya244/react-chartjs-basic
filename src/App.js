e2b7b5eeb2fc763c821371dea128de08d93e72f9
Aditya <sinha.aditya244@gmail.com>
2020-05-06 14:05:34 +0530

    Basic react app for demo of Chart JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Pie, Doughnut} from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const state = {
+  labels: ['January', 'February', 'March',
+           'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: [
+        '#B21F00',
+        '#C9DE00',
+        '#2FDE00',
+        '#00A6B4',
+        '#6800B4'
+      ],
+      hoverBackgroundColor: [
+      '#501800',
+      '#4B5000',
+      '#175000',
+      '#003350',
+      '#35014F'
+      ],
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="text-center"> Chart JS App</h1>
+        <div className="data-div col-md-6 float-left">
+        <Pie
+          data={state}
+          // width={"500px"}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'left',
+            }
+          }}
+        />
+        </div>
+        <div className="data-div col-md-6 float-left">
+        <Doughnut
+          // width={"500px"}
+          data={state}
+          options={{
+            title:{
+              display:true,
+              text:'Average Rainfall per month',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'left'
+            },
+            responsive: true,
+            maintainAspectRatio: true
+          }}
+        />
+      </div>
+      </div>
+    );
+  }
+}
